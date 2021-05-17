6bed347e136f653cd373e38104d1c3fc193030b4
abubillo1991 <abubillo1991@gmail.com>
2021-05-17 16:38:12 +0200

    .

--- js/js_core.js
+++ js/js_core.js
@@ -10,3 +10,7 @@ for (let _category of _categories) {
     // <!-- *** 004 - Categorias -->
     add_ul_category_004(_category['id'],_category['name'],_category['url']);
 }
+
+for (let _article of _articles) {
+    add_article_card(_article['id'], _article['name'], _article['category_id'], _article['intro_description'], _article['date'], _article['score'], _article['status'], _article['image'], _article['price']);
+}
--- js/js_data.js
+++ js/js_data.js
@@ -1,4 +1,9 @@
-_articles = [{ id: 0, name: "Alien Worlds - Farmea TLM", url: "#", image: "https://s2.coinmarketcap.com/static/img/coins/200x200/9119.png", intro_description: 'Wisi forensibus mnesarchum in cum. Per id impetus abhorreant, his no <b>magna</b> definiebas, inani rationibus in quo. Ut vidisse dolores est, ut quis nominavi mel', price: "Gratis", category_id: 0 }];
-_name = 'a';
-_categories = [{ id: 0, name: "Gratis", url: "#" },{ id: 1, name: "Juegos", url:"#"},{ id: 2, name: "Oportunidades", url:"#"}];
+_articles = [
+    { id: 0, name: "Alien Worlds - Farmea TLM", status: 1, published_date: '17-05-2021', score: 4.5, url: "#", image: "https://s2.coinmarketcap.com/static/img/coins/200x200/9119.png", price: "Gratis", category_id: 0 , intro_description: 'Wisi forensibus mnesarchum in cum. Per id impetus abhorreant, his no <b>magna</b> definiebas, inani rationibus in quo. Ut vidisse dolores est, ut quis nominavi mel'}
+];
+_categories = [
+    { id: 0, name: "Gratis", url: "#" },
+    { id: 1, name: "Juegos", url: "#" },
+    { id: 2, name: "Oportunidades", url: "#" }
+];
 
