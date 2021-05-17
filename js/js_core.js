46f9f2db273931d4af0f05cc0eb7a8574cceb429
abubillo1991 <abubillo1991@gmail.com>
2021-05-17 13:22:51 +0200

    .

---
+++ js/js_core.js
@@ -0,0 +1,1 @@
+add_nav_category(_categories[0]['name']);
--- js/js_data.js
+++ js/js_data.js
@@ -1,6 +1,4 @@
 _articles = [{ id: 0, name: "Alien Worlds - Farmea TLM", url: "#", image: "https://s2.coinmarketcap.com/static/img/coins/200x200/9119.png", intro_description: 'Wisi forensibus mnesarchum in cum. Per id impetus abhorreant, his no <b>magna</b> definiebas, inani rationibus in quo. Ut vidisse dolores est, ut quis nominavi mel', price: "Gratis", category_id: 0 }];
 _name = 'a';
-_categories = [{ id: 0, name: "Categoria A", url: "#" }];
-add_nav_category(_categories[0]['name']);
-
+_categories = [{ id: 0, name: "Gratis", url: "#" },{ id: 1, name: "Juegos", url:"#"},{ id: 2, name: "Oportunidad", url:"#"}];
 
