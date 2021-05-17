2252a67eb0dc530016778f07e3110ec33e94a8dd
abubillo1991 <abubillo1991@gmail.com>
2021-05-17 15:24:12 +0200

    .

--- js/js_core.js
+++ js/js_core.js
@@ -5,4 +5,6 @@ for (let _category of _categories) {
     add_nav_category_001(_category['id'],_category['name'],_category['url']);
     // <!-- *** 002 - Categorias -->
     add_select_category_002(_category['id'],_category['name'],_category['url']);
+    // <!-- *** 003 - Categorias -->
+    add_ul_category_003(_category['id'],_category['name'],_category['url']);
 }
