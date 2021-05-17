8a8755650489a7107743b36592e7d210442eec1c
abubillo1991 <abubillo1991@gmail.com>
2021-05-17 16:42:38 +0200

    ..

--- js/js_methods.js
+++ js/js_methods.js
@@ -28,7 +28,7 @@ function add_article_card(id, name, category_id, intro_description, date, score,
 
   _article_card += '<div class="product-card__rating-legend">'+score+'</div></div><p class="product-card__features-list">' + intro_description + '</p></div><div class="product-card__actions"><div class="product-card__availability">Estado: ';
 
-  if(score==1){_article_card += '<span class="text-success">Disponible';}else{_article_card += '<span class="text-danger">Agotado';}
+  if(status==1){_article_card += '<span class="text-success">Disponible';}else{_article_card += '<span class="text-danger">Agotado';}
   _article_card += '</span></div><div class="product-card__prices">' + price + '</div><div class="product-card__buttons"><a class="btn btn-secondary product-card__addtocart product-card__addtocart--list" type="button" href="chollo.html?id='+id+'">Ir al chollo</a></div></div></div>';
   
   document.getElementById("_article_card_list").innerHTML += _article_card 
