a2e251834d5e60e904bbc8e82c992403a43349ac
abubillo1991 <abubillo1991@gmail.com>
2021-05-18 10:57:15 +0200

    Update js_methods.js

--- js/js_methods.js
+++ js/js_methods.js
@@ -23,7 +23,7 @@ function filter_and_order_articles(_articles, c_id, q, s, o) {
   }
 
   if (q != null) {
-    console.log(_articles[0]['description'].includes("Wisi"))
+    console.log(_articles[0]['description'].includes("Wisi"));
     _articles = _articles.filter(({ description }) => description.includes(q) == true);
     console.log("5entra");
   }
