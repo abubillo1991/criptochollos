d960e0a35cbb4d994ad2b9284a3c46732995d97c
abubillo1991 <abubillo1991@gmail.com>
2021-05-24 13:49:56 +0200

    Update js_oferta_core.js

--- js/js_oferta_core.js
+++ js/js_oferta_core.js
@@ -18,19 +18,23 @@ change_breadcrumb(get_category_name_by_id(_article['category_id']),_article['cat
 
 update_offer(_article['id'], _article['name'], _article['category_id'], _article['intro_description'], _article['date'], _article['score'], _article['status'], _article['image'], _article['price'], _article['url'], _article['user'], _article['description'])
 
+for (let _category of _categories) {
+    // <!-- *** 004 - Categorias -->
+    add_ul_category_004(_category['id'], _category['name'], _category['url']);
+}
 
 /*
 update_search_filters(o,s);
 
 for (let _category of _categories) {
+    
+    
     // <!-- *** 001 - Categorias -->
     add_nav_category_001(_category['id'], _category['name'], _category['url']);
     // <!-- *** 002 - Categorias -->
     add_select_category_002(_category['id'], _category['name'], _category['url']);
     // <!-- *** 003 - Categorias -->
     add_ul_category_003(_category['id'], _category['name'], _category['url']);
-    // <!-- *** 004 - Categorias -->
-    add_ul_category_004(_category['id'], _category['name'], _category['url']);
     // <!-- *** 005 - Categorias -->
     add_div_category_005(_category['id'], _category['name'], _category['url']);
 }
