2c2b6995ef5466bf3a616afbdc9aec5ab091ceb4
abubillo1991 <abubillo1991@gmail.com>
2021-05-24 12:15:05 +0200

    .

--- js/js_methods.js
+++ js/js_methods.js
@@ -88,7 +88,7 @@ function add_widget_card(id, name, category_id, status, image, price, url) {
   document.getElementById("_widget_card_list").innerHTML += _article_card;
 }
 
-/*
+
 function update_search_filters(o,s){
   s_options='<option selected value="1">Disponible</option><option value="0">Agotado</option>';
   if(s != null && s==0){
@@ -101,7 +101,7 @@ function update_search_filters(o,s){
     s_options='<option value="date">Fecha de publicación</option><option selected value="score">Relevancia</option>';
   }
   document.getElementById("o").innerHTML += o_options;
-}*/
+}
 
 function get_article_by_id(id){
   return _articles[id];
