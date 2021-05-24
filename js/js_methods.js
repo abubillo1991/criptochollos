a1ecb56aa865dbda94722f6c938f718e9ea6bed6
abubillo1991 <abubillo1991@gmail.com>
2021-05-24 10:58:00 +0200

    Update js_methods.js

--- js/js_methods.js
+++ js/js_methods.js
@@ -58,12 +58,12 @@ function add_div_category_005(id, name, url) {
 
 function add_article_card(id, name, category_id, intro_description, date, score, status, image, price, ad) {
   console.log(date+"/"+new Date(date))
-  _article_card = '<div class="product-card product-card--hidden-actions ">';
+  _article_card = '<div class="products-list__item"><div class="product-card product-card--hidden-actions ">';
 
   if (score > 4.5) { _article_card += '<div class="product-card__badges-list"><div class="product-card__badge product-card__badge--sale">HOT</div></div>'; }
   else if (((new Date) - new Date(date)) <  (60 * 60 * 1000 * 24 * 7)){ _article_card += '<div class="product-card__badges-list"><div class="product-card__badge product-card__badge--new">NUEVO</div></div>'; }
 
-  _article_card += '<div class="products-list__item"><div class="product-card__image product-image"><a target="_blank" href="oferta.html?id=' + id + '" class="product-image__body"><img class="product-image__img" src="' + image + '" alt=""></a></div><div class="product-card__info"><div class="product-card__name"><a target="_blank" href="oferta.html?id=' + id + '">' + name + '</a></div><div class="product-card__rating"><div class="product-card__rating-stars"><div class="rating"><div class="rating__body">';
+  _article_card += '<div class="product-card__image product-image"><a target="_blank" href="oferta.html?id=' + id + '" class="product-image__body"><img class="product-image__img" src="' + image + '" alt=""></a></div><div class="product-card__info"><div class="product-card__name"><a target="_blank" href="oferta.html?id=' + id + '">' + name + '</a></div><div class="product-card__rating"><div class="product-card__rating-stars"><div class="rating"><div class="rating__body">';
   for (i = 1; i <= 5; i++) {
     if (i <= score) { _article_card += '<svg class="rating__star rating__star--active" width="13px" height="12px"><g class="rating__fill"><use xlink:href="images/sprite.svg#star-normal"></use></g><g class="rating__stroke"><use xlink:href="images/sprite.svg#star-normal-stroke"></use></g></svg><div class="rating__star rating__star--only-edge rating__star--active"><div class="rating__fill"><div class="fake-svg-icon"></div></div><div class="rating__stroke"><div class="fake-svg-icon"></div></div></div>'; }
     else { _article_card += '<svg class="rating__star " width="13px" height="12px"><g class="rating__fill"><use xlink:href="images/sprite.svg#star-normal"></use></g><g class="rating__stroke"><use xlink:href="images/sprite.svg#star-normal-stroke"></use></g></svg><div class="rating__star rating__star--only-edge "><div class="rating__fill"><div class="fake-svg-icon"></div></div><div class="rating__stroke"><div class="fake-svg-icon"></div></div></div>'; }
