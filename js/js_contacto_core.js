c10b6a21e9414c093a0fa7153b6dba8d13c3dabb
abubillo1991 <abubillo1991@gmail.com>
2021-05-25 12:48:05 +0200

    a

--- js/js_contacto_core.js
+++ js/js_contacto_core.js
@@ -12,7 +12,11 @@ if (sent==1){
     document.getElementById("contact-alert").innerHTML += '<div class="alert alert-primary alert-lg mb-3 alert-dismissible fade show"><i class="fas fa-check"></i> El mensaje ha sido enviado con éxito. <button type="button" class="close" data-dismiss="alert" aria-label="Close"><svg width="12px" height="12px"><use xlink:href="images/sprite.svg#cross-12"></use></svg></button></div>'
 }
 
-function contact_form() {
-    c_message='n: '+document.getElementById("form-name").value + ', e: '+document.getElementById("form-email").value + ', s: '+document.getElementById("form-subject").value + ', m: '+document.getElementById("form-message").value;
+function contact_form(site) {
+    if (site == 'publicar-chollo'){
+        c_message='n: '+document.getElementById("form-name").value + ', e: '+document.getElementById("form-email").value + ', s: '+document.getElementById("form-subject").value + ', m: '+document.getElementById("form-message").value;
+    }else{
+        c_message='n: '+document.getElementById("form-name").value + ', e: '+document.getElementById("form-email").value + ', s: '+document.getElementById("form-subject").value + ', m: '+document.getElementById("form-message").value;
+    }
     fetch_contact(c_message)
   }
