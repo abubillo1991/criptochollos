293c466b84844ab230717bbfc5e8b8f2ee0d597d
abubillo1991 <abubillo1991@gmail.com>
2021-05-25 13:20:20 +0200

    .

--- js/js_contacto_core.js
+++ js/js_contacto_core.js
@@ -14,13 +14,13 @@ if (sent==1){
 
 function contact_form(site) {
     if (site == 'publicar-chollo'){
-        c_message='n: '+document.getElementById("form-name").value + ', e: '+document.getElementById("form-email").value + ', s: '+document.getElementById("form-subject").value + ', m: '+document.getElementById("form-message").value;
+        c_message='n: '+document.getElementById("form-name").value+',r: '+document.getElementById("form-ref").value+',c: '+document.getElementById("form-code").value+',u: '+document.getElementById("form-user").value+',c_id: '+document.getElementById("form-category_id").value+',d: '+document.getElementById("form-description").value;
     }else{
         c_message='n: '+document.getElementById("form-name").value + ', e: '+document.getElementById("form-email").value + ', s: '+document.getElementById("form-subject").value + ', m: '+document.getElementById("form-message").value;
     }
     fetch_contact(c_message);
 
-    sleep(1000);
+    sleep(500);
     window.location.replace("?sent=1");
   }
 
